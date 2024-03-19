0f6af418c2748f8dc473be4b01337e92d0e0104f
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-03-19 13:01:10 +0100

    changed email to sub

--- client/src/pages/FavouriteImages.tsx
+++ client/src/pages/FavouriteImages.tsx
@@ -1,52 +1,56 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 interface IFavoriteImages {
-    imageLink: string
+  imageLink: string;
 }
 
 export const FavouriteImages = () => {
-    const [favoriteImages, setFavoriteImages] = useState<IFavoriteImages[]>([]);
+  const [favoriteImages, setFavoriteImages] = useState<IFavoriteImages[]>([]);
 
-    const {user} = useAuth0();  
-    
-    useEffect(() => {
-        const fetchFavoriteImages = async () => {
-            try {
-                const response = await fetch(`http://localhost:3000/api/favouriteImages/${user?.email}`, {
-                    method:'GET',
-                    headers:{
-                        'Content-Type':'application/json',
-                    },
-                })
-                if (!response.ok) {
-                    throw new Error("error")
-                }
-                const data = await response.json();     
-              
-                if (data.length > 0) {
-                    setFavoriteImages(data[0].myFavImages || []);
-                } else {
-                    setFavoriteImages([]);
-                }
-                       
-                // setFavoriteImages(data.myFavImages || [])
-            } catch (error) {
-                console.error("error", error);
-                
-            }
+  const { user } = useAuth0();
+
+  useEffect(() => {
+    const fetchFavoriteImages = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/favouriteImages/${user?.sub}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("error");
         }
-        fetchFavoriteImages(); 
-    }, [user?.email]); 
+        const data = await response.json();
 
-    return (
+        if (data.length > 0) {
+          setFavoriteImages(data[0].myFavImages || []);
+        } else {
+          setFavoriteImages([]);
+        }
 
-        <div>
-          {favoriteImages && favoriteImages.map((image, index) => (
-            <img key={index} src={image.imageLink} alt={`Favorite Image ${index}`} />
-          ))}
-        </div>
- 
-    )
+        // setFavoriteImages(data.myFavImages || [])
+      } catch (error) {
+        console.error("error", error);
+      }
+    };
+    fetchFavoriteImages();
+  }, [user?.sub]);
 
-}
+  return (
+    <div>
+      {favoriteImages &&
+        favoriteImages.map((image, index) => (
+          <img
+            key={index}
+            src={image.imageLink}
+            alt={`Favorite Image ${index}`}
+          />
+        ))}
+    </div>
+  );
+};
